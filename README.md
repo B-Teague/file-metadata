89ee588f6f314945db2ec756f1f29f7b129ecb71
Glitch (teague-apis-file-metadata) <none>
2019-01-29 22:12:14 +0000

    🔪🚡 Updated with Glitch

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 Teague APIs - File Metadata Microservice
 ====================================
 
-This API will return the size metadata of an uploaded file.
+This API will return the metadata of an uploaded file.
 
 Example for invoking
 --------------------
 
-Select a file to upload from your PC and the API will return the total size of the file.
+Select a file to upload from your PC and the API will return the metadata of the file.
--- server.js
+++ server.js
@@ -1,32 +1,13 @@
-// server.js
-// where your node app starts
+const express = require('express');
+const app = express();
+const indexRoute = require(__dirname + '/routes/index');
+const uploadRoute = require(__dirname + '/routes/upload');
 
-// init project
-var express = require('express');
-var multer = require('multer');
-var storage = multer.memoryStorage();
-var app = express();
-var upload = multer({ storage: storage });
-//var storage = multer.memoryStorage();
-//var upload = multer({ storage: storage });
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
+app.set('json spaces', 2);
 app.use(express.static('public'));
+app.use('/', indexRoute);
+app.use('/upload', uploadRoute);
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.post("/upload",upload.single('testFile'), function (request, response) {
-  response.send({size: request.file.size});
-
-});
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
